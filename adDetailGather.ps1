db61af460b8428541f7687debbcfe0abb29ff819
oldManLemon <oldm8reggie@gmail.com>
2019-11-06 09:18:45 +0100

    ready for testing

--- adDetailGather.ps1
+++ adDetailGather.ps1
@@ -1,6 +1,6 @@
 
 $me = Get-ADUser -Identity euser -Properties *
-$potter = Get-ADUser -Identity ahase -Properties "UserPrincipalName","MemberOf", "ProfilePath","CN", "City", "c","Country","l","mail","mailNickname","st","State","Department","Description"
+$potter = Get-ADUser -Identity ahase -Properties "UserPrincipalName","MemberOf", "ProfilePath","CN", "City", "c","Country","l","mail","mailNickname","st","State","Department","Description","Title"
 
 # $me = Get-ADUser -Identity ahase -Properties "UserPrincipalName","MemberOf", "ProfilePath"
 #$p = Get-ADUser -Identity hpotter -Properties "UserPrincipalName","MemberOf", "ProfilePath"
@@ -25,6 +25,6 @@ $potter = Get-ADUser -Identity ahase -Properties "UserPrincipalName","MemberOf",
 # }
 # $newPath
 # $me
-#$potter.Title
-$me.Title
+$potter
+#$me.Title
 
--- copyUsr.ps1
+++ copyUsr.ps1
@@ -1,25 +1,19 @@
 
-
 $usrToCopy = Read-Host -Prompt 'Please enter the user you wish to copy here'
 $firstName, $lastName = $usrToCopy.Split(' ')
-
-#$defaultPassword = 'Start#2019' 
 $defaultPassword = ConvertTo-SecureString -String "Start#2019" -AsPlainText -Force
-#$defaultPassword=ConvertTo-SecureString 'MySuperSecretP@ssw0rd!'
-
-# #For testing
-# $firstName = 'Andrew'
-# $lastName = 'Hase'
-# Write-Host "You are searching firstname'$firstName'"
-
 $filterForAdSearch = "givenName -like ""*$firstName*"" -and sn -like ""$lastName"""
 
 $user = try {
-    Get-ADUser -Filter $filterForAdSearch -Properties "UserPrincipalName","MemberOf", "ProfilePath","CN", "City", "c","Country","l","mail","mailNickname","st","State","Department","Description"
+    Get-ADUser -Filter $filterForAdSearch -Properties "UserPrincipalName","MemberOf", "ProfilePath","CN", "City", "c","Country","l","mail","mailNickname","st","State","Department","Description","Title"
     Write-Host 'User' $usrToCopy 'will now be your template'
 }
 catch {
     Write-Host "Please check again, user is not found. 'n Is this user in Stuttgart?"
+}
+if($user -is [array] ){
+    Write-Host 'There is has being an err, your selected user to copy has showed up more than once, it is an array not an obj. Breaking'
+    break
 }
 $userInstance = Get-ADUser -Identity $user.SamAccountName
 
@@ -56,12 +50,15 @@ IF ($user) {
     $newUsr = Read-Host -Prompt 'Enter name of new user'
 
     $newFirstName, $newLastName = $newUsr.Split()
+    #Flip the user names to match all other users
+    $newUsr = $newLastName+', '+$newFirstName
+    #$newUsr
     #Create user login
     $newSamAccountName = ($newFirstName[0] + $newLastName).ToLower()
     #check to see if user login already exists and create a new version if exists
     #Example Tim Burton is tburton however if that exists it will create tiburton and timburton and so on and so fourth
     $newSamAccountName = checkUsrSam -samName $newSamAccountName -fName $newFirstName -lName $newLastName -run 1
-    
+    #keep user unmodified
     $stringMod = $user
     $frontGarbage, $endPrincipal = $stringMod.UserPrincipalName.Split('@')
     # Create a new principle name for login in modern Systems
@@ -70,6 +67,7 @@ IF ($user) {
     $s = $stringMod.DistinguishedName
     # Getting user into the correct OU by generating path
     $sectionOneGarbage, $sectionTwoGarbage, $sectionThree = $s.Split(',')
+    $displayName = $newLastName+', '+$newFirstName
     $newPath
     $noComma = $sectionThree.Length
     $i = 1
@@ -81,14 +79,36 @@ IF ($user) {
         }
         $i++
     }
-    $newPath
+    
+    $params = @{
+        "Instance"=$userInstance
+        "Name"=$newUsr
+        "DisplayName"= $displayName
+        "GivenName"=$newFirstName
+        "SurName"=$newLastName
+        "AccountPassword"=$defaultPassword
+        "Enabled"=$enabled
+        "ChangePasswordAtLogon"=$true
+        "UserPrincipalName" = $newUsrPrincipalName
+        #Here are the details
+        "c" = $user.c
+        "City" = $user.City
+        "Country" = $user.Country
+        "Department" = $user.Department
+        "Discription" = $user.Discription
+
+
+    }
+    #$newPath
 
     
-    # -UserPrincipalName $newUsrPrincipalName
+   #Create New User
+
+   #New-ADUser $params -WhatIf
    
     
 
-    New-ADUser -Name $newUsr -SamAccountName $newSamAccountName -Path $newPath -ChangePasswordAtLogon $True -AccountPassword $defaultPassword -GivenName $newFirstName -Surname $newLastName -DisplayName $newLastName', '$newFirstName -UserPrincipalName $newUsrPrincipalName -Instance $userInstance -WhatIf
+    # New-ADUser -Name $newUsr -SamAccountName $newSamAccountName -Path $newPath -ChangePasswordAtLogon $True -AccountPassword $defaultPassword -GivenName $newFirstName -Surname $newLastName -DisplayName $newLastName', '$newFirstName -UserPrincipalName $newUsrPrincipalName -Instance $userInstance -WhatIf
     # New-ADUser -Name $newUsr -SamAccountName $newSamAccountName -ChangePasswordAtLogon $True -AccountPassword $defaultPassword -Instance $user -WhatIf
 
 }
