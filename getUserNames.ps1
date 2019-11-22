cfc97170566d30a3205e7985ef69042dd98d8ffa
oldManLemon <oldm8reggie@gmail.com>
2019-11-22 15:24:17 +0100

    Cleanup of getnames

--- getUserNames.ps1
+++ getUserNames.ps1
@@ -20,51 +20,5 @@ if(!$testPath){
     New-Item -Path $appDataLocation -Name "usrData" -ItemType "directory"
 }
 #Get CSV with Full Display Names and account names
-#Get-ADUser -Server $server -Filter '*' -Properties DisplayName, Samaccountname | select DisplayName, Samaccountname | Export-Csv $userDataLocation$server".csv"
-#Get-ADUser -Server $server -Filter '*' -Properties DisplayName, Samaccountname | select DisplayName, Samaccountname | Out-File $userDataLocation$server".txt"
+
 Get-ADUser -Server $server -Filter '*' -Properties DisplayName, Samaccountname | select DisplayName, Samaccountname | Format-List DisplayName, Samaccountname | Out-File $server".txt"
-
-# $name,$sam = $user.DisplayName,$user.Samaccountname 
-# $name.GetType().FullName
-# for($i = 0 
-# $i -le $sam.Count
-# $i++){
-#     $string = $user[$i] +":"+ $sam[$i]
-#     Add-Content -Path $server".txt" -Encoding "utf8" -Value $string
-
-# }
-
-
-
-
-# param(
-# [string]$Server
-# ) 
-
-# if (!$Server)
-# {
-#     $Server = $env:computername 
-# }
-
-# Get-ADUser -Server $server -Filter '*' -Properties DisplayName, Samaccountname | select DisplayName, Samaccountname | Export-Csv $server'.csv' 
-
-
-
-# param(
-# [string]$Server
-# ) 
-
-# if (!$Server)
-# {
-#     $Server = $env:computername 
-# }
-
-# $fileLocation = 'C:\Users\ahase\source\repos\poshScripts\userData\'+$server+'.csv' 
-
-
-
-# Get-ADUser -Server $server -Filter '*' -Properties DisplayName, Samaccountname | select DisplayName, Samaccountname | Export-Csv $fileLocation
-
-
-
-
